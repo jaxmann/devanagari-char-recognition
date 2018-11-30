39c5942e54f4257e44c96729c84a250eeb719a5c
Jonathan Axmann <jaxmann@gatech.edu>
2018-11-30 15:51:24 -0500

    update vgg19 model

--- models/vgg19.py
+++ models/vgg19.py
@@ -21,7 +21,7 @@ class VGG19(nn.Module):
         #############################################################################
 
         # this is pretty much VGG11
-        self.model = nn.Sequential(
+        self.features = nn.Sequential(
 
             nn.Conv2d(3, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
@@ -96,11 +96,23 @@ class VGG19(nn.Module):
             nn.ReLU(inplace=True),
 
             #changed final average pooling size for simpler linear layer
-            nn.AvgPool2d(kernel_size=2, stride=2)
+            # nn.AvgPool2d(kernel_size=2, stride=2)
 
         )
 
-        self.linear = nn.Linear(512, n_classes)
+        self.classifier = nn.Sequential(
+            nn.Linear(512 * 7 * 7, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, n_classes),
+        )
+        # if init_weights:
+        self._initialize_weights()
+
+        # self.linear = nn.Linear(512, n_classes)
 
         # self.n_classes = n_classes
         #
@@ -157,12 +169,17 @@ class VGG19(nn.Module):
 
         # x = self.pool1(images)
 
-        out = self.model(images)
-        # print(out.shape)
-        out = out.view(-1, 512)
-        # out = out.view(out.size(0), -1)
-        scores = self.linear(out)
-        return scores
+        x = self.features(images)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+        # out = self.model(images)
+        # # print(out.shape)
+        # out = out.view(-1, 512)
+        # # out = out.view(out.size(0), -1)
+        # scores = self.linear(out)
+        # return scores
 
         # x = self.conv1(images)
         # x = F.relu(self.bn1(x))
@@ -191,5 +208,18 @@ class VGG19(nn.Module):
         #############################################################################
         #                             END OF YOUR CODE                              #
         #############################################################################
-        return scores
+        # return scores
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
 
