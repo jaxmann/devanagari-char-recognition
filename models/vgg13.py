fe6377dd5868ae94075c285f173ca597eab5feb2
Joshi, Mihir Shrirang <mjoshi60@gatech.edu>
2018-12-01 14:32:10 -0500

    Added VGG13

---
+++ models/vgg13.py
@@ -0,0 +1,74 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MyModel(nn.Module):
+    def __init__(self, im_size, hidden_dim, kernel_size, n_classes):
+        '''
+        Extra credit model
+
+        Arguments:
+            im_size (tuple): A tuple of ints with (channels, height, width)
+            hidden_dim (int): Number of hidden activations to use
+            kernel_size (int): Width and height of (square) convolution filters
+            n_classes (int): Number of classes to score
+        '''
+        super(MyModel, self).__init__()
+        #############################################################################
+        # TODO: Initialize anything you need for the forward pass
+        #############################################################################
+        layers = []
+        layers += [nn.Conv2d(3, 64, kernel_size=3, padding=1), nn.BatchNorm2d(64), nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.BatchNorm2d(64), nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.BatchNorm2d(128), nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(128, 128, kernel_size=3, padding=1), nn.BatchNorm2d(128), nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Conv2d(128, 256, kernel_size=3, padding=1), nn.BatchNorm2d(256), nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(256, 256, kernel_size=3, padding=1), nn.BatchNorm2d(256), nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Conv2d(256, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.Conv2d(512, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(inplace=True)]
+        layers += [nn.Conv2d(512, 512, kernel_size=3, padding=1), nn.BatchNorm2d(512), nn.ReLU(inplace=True)]
+        layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        self.vgg11 = nn.Sequential(*layers)
+        self.sm = nn.Linear(512,10)
+#         print(layers)
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+
+    def forward(self, images):
+        '''
+        Take a batch of images and run them through the model to
+        produce a score for each class.
+
+        Arguments:
+            images (Variable): A tensor of size (N, C, H, W) where
+                N is the batch size
+                C is the number of channels
+                H is the image height
+                W is the image width
+
+        Returns:
+            A torch Variable of size (N, n_classes) specifying the score
+            for each example and category.
+        '''
+        scores = None
+        #############################################################################
+        # TODO: Implement the forward pass.
+        #############################################################################
+        out = self.vgg11(images)
+#         print(out.shape)
+        scores = self.sm(out.view(-1, 512))
+                      
+        #############################################################################
+        #                             END OF YOUR CODE                              #
+        #############################################################################
+        return scores
+
